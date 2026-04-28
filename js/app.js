31a2f007f94777c9726ba5fa7aaebfd6bd008356
Sara Abigail Sulca Zuloaga <samihimezu@gmail.com>
2026-04-27 23:53:51 -0500

    RF-03: Agregando lógica al RF

--- js/app.js
+++ js/app.js
@@ -14,3 +14,19 @@ form.addEventListener("submit", function (e) {
   list.appendChild(li);
   input.value = "";
 });
+const form = document.getElementById("taskForm");
+const input = document.getElementById("taskInput");
+const list = document.getElementById("taskList");
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  const taskText = input.value.trim();
+  if (taskText === "") return;
+
+  const li = document.createElement("li");
+  li.textContent = taskText;
+
+  list.appendChild(li);
+  input.value = "";
+});
